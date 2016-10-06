a5f62f0385d3025bf22d0f3a22fab6da290b4290
Tarek Said <tarek.said.info@gmail.com>
2016-10-06 19:50:47 -0300

    Returning HTTP status 404 when the business is not found

--- businesses-service/businesses-impl/src/main/java/co/synergyspace/businesses/exceptions/impl/BusinessNotFoundException.java
+++ businesses-service/businesses-impl/src/main/java/co/synergyspace/businesses/exceptions/impl/BusinessNotFoundException.java
@@ -1,10 +1,13 @@
 package co.synergyspace.businesses.exceptions.impl;
 
 import co.synergyspace.businesses.exceptions.BusinessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  * Created by tarek on 05/10/16.
  */
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class BusinessNotFoundException extends BusinessException {
 
     public BusinessNotFoundException(String name) {
