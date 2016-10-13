e6219562138ab93e65d5fa382ee2e91a9071cc47
Tarek Said <tarek.said.info@gmail.com>
2016-10-12 22:46:31 -0300

    cleaning up code

--- posts-service/posts-impl/src/main/java/co/synergyspace/posts/AppConfig.java
+++ posts-service/posts-impl/src/main/java/co/synergyspace/posts/AppConfig.java
@@ -2,18 +2,15 @@ package co.synergyspace.posts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by tarek on 26/09/16.
  */
 @EnableTransactionManagement
-@EnableNeo4jRepositories("co.synergyspace.posts.repositories")
+@EnableNeo4jRepositories
 @SpringBootApplication
 @EnableEurekaClient
 public class AppConfig {
